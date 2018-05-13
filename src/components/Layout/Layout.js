64b8ddfbcca3d9c01b63f26ff4e86f3d555b912f
payameshtiagh <payam.eshtiagh@gmail.com>
2018-05-13 13:52:46 +0200

    first draft

---
+++ src/components/Layout/Layout.js
@@ -0,0 +1,34 @@
+import React, { Component } from "react";
+import Auxx from "../../hoc/Auxx";
+import Toolbar from "../Navigation/Toolbar/Toolbar";
+import classes from "./Layout.css";
+import SideDrawer from "../Navigation/SideDrawer/SideDrawer";
+
+class Layout extends Component{
+  state = {
+    showSideDrawer: false
+  }
+
+  sideDrawerClosedHandler = () =>{
+    this.setState({showSideDrawer: false});
+  }
+  sideDrawerToggleHandler = () => {
+    this.setState((prevState)=>{
+    return  {showSideDrawer: !this.state.showSideDrawer};
+    });
+  }
+
+  render(){
+    return (  <Auxx>
+      <Toolbar drawerToggleClicked={this.sideDrawerToggleHandler} />
+      <SideDrawer
+        open={this.state.showSideDrawer}
+        closed={this.sideDrawerClosedHandler} />
+        <main className={classes.Content}>
+          {this.props.children}
+          </main>
+        </Auxx>)
+  }
+}
+
+export default Layout;
